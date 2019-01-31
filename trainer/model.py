c435f5a0e3ade7252b8684b61631c4351ddb31e1
Achyut Boggaram <achyut.boggaram@clobotics.com>
2019-01-31 12:02:09 -0800

    fixed indentation errors

--- trainer/model.py
+++ trainer/model.py
@@ -54,117 +54,117 @@ EPOCH_SIZE = 60000
 
 
 def adjust_image(data):
-    # Reshape to [batch, height, width, channels].
-    imgs = tf.reshape(data, [-1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS])
-    # Adjust image size to Inception-v3 input.
-    imgs = tf.image.resize_images(imgs, (256, 256))
-    # Convert to RGB image.
-    imgs = tf.image.grayscale_to_rgb(imgs)
-    return imgs
+        # Reshape to [batch, height, width, channels].
+        imgs = tf.reshape(data, [-1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS])
+        # Adjust image size to Inception-v3 input.
+        imgs = tf.image.resize_images(imgs, (256, 256))
+        # Convert to RGB image.
+        imgs = tf.image.grayscale_to_rgb(imgs)
+        return imgs
 
 def read_and_decode(filename_queue):
-  reader = tf.TFRecordReader()
-  _, serialized_example = reader.read(filename_queue)
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
 
-  features = tf.parse_single_example(
-      serialized_example,
-      features={
-          'image_raw': tf.FixedLenFeature([], tf.string),
-          'label': tf.FixedLenFeature([], tf.int64),
-      })
+    features = tf.parse_single_example(
+            serialized_example,
+            features={
+                    'image_raw': tf.FixedLenFeature([], tf.string),
+                    'label': tf.FixedLenFeature([], tf.int64),
+            })
 
-  image = tf.decode_raw(features['image_raw'], tf.uint8)
-  image.set_shape([IMAGE_FLATTENED_SIZE])
-  image = tf.cast(image, tf.float32) * (1. / 255)
-  label = tf.cast(features['label'], tf.int32)
+    image = tf.decode_raw(features['image_raw'], tf.uint8)
+    image.set_shape([IMAGE_FLATTENED_SIZE])
+    image = tf.cast(image, tf.float32) * (1. / 255)
+    label = tf.cast(features['label'], tf.int32)
 
-  return image, label
+    return image, label
 
 
 def input_fn(filename, batch_size=100):
-  filename_queue = tf.train.string_input_producer([filename])
+    filename_queue = tf.train.string_input_producer([filename])
 
-  image, label = read_and_decode(filename_queue)
-  images, labels = tf.train.batch(
-      [image, label], batch_size=batch_size,
-      capacity=1000 + 3 * batch_size)
+    image, label = read_and_decode(filename_queue)
+    images, labels = tf.train.batch(
+            [image, label], batch_size=batch_size,
+            capacity=1000 + 3 * batch_size)
 
-  return {'inputs': images}, labels
+    return {'inputs': images}, labels
 
 
 def get_input_fn(filename, batch_size=100):
-  return lambda: input_fn(filename, batch_size)
+    return lambda: input_fn(filename, batch_size)
 
 
 
 def _cnn_model_fn(features, labels, mode):
-  # Input Layer
-  input_layer = tf.reshape(features['inputs'], [-1, IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS])
+    # Input Layer
+    input_layer = tf.reshape(features['inputs'], [-1, IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS])
 
-  # Convolutional Layer #1
+    # Convolutional Layer #1
 
-  conv1 = tf.layers.conv2d(
-      inputs=input_layer,
-      filters=32,
-      kernel_size=[5, 5],
-      padding='same',
-      activation=tf.nn.relu)
+    conv1 = tf.layers.conv2d(
+            inputs=input_layer,
+            filters=32,
+            kernel_size=[5, 5],
+            padding='same',
+            activation=tf.nn.relu)
 
-  # Pooling Layer #1
-  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+    # Pooling Layer #1
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
 
-  # Convolutional Layer #2 and Pooling Layer #2
-  conv2 = tf.layers.conv2d(
-      inputs=pool1,
-      filters=64,
-      kernel_size=[5, 5],
-      padding='same',
-      activation=tf.nn.relu)
-  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+    # Convolutional Layer #2 and Pooling Layer #2
+    conv2 = tf.layers.conv2d(
+            inputs=pool1,
+            filters=64,
+            kernel_size=[5, 5],
+            padding='same',
+            activation=tf.nn.relu)
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
-  # Dense Layer
-  pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
-  dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
-  dropout = tf.layers.dropout(
-      inputs=dense, rate=0.4, training=(mode == Modes.TRAIN))
+    # Dense Layer
+    pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+    dropout = tf.layers.dropout(
+            inputs=dense, rate=0.4, training=(mode == Modes.TRAIN))
 
-  # Logits Layer
-  logits = tf.layers.dense(inputs=dropout, units=10)
+    # Logits Layer
+    logits = tf.layers.dense(inputs=dropout, units=10)
 
-  # Define operations
-  if mode in (Modes.INFER, Modes.EVAL):
-    predicted_indices = tf.argmax(input=logits, axis=1)
-    probabilities = tf.nn.softmax(logits, name='softmax_tensor')
+    # Define operations
+    if mode in (Modes.INFER, Modes.EVAL):
+        predicted_indices = tf.argmax(input=logits, axis=1)
+        probabilities = tf.nn.softmax(logits, name='softmax_tensor')
 
-  if mode in (Modes.TRAIN, Modes.EVAL):
-    global_step = tf.contrib.framework.get_or_create_global_step()
-    label_indices = tf.cast(labels, tf.int32)
-    loss = tf.losses.softmax_cross_entropy(
-        onehot_labels=tf.one_hot(label_indices, depth=10), logits=logits)
-    tf.summary.scalar('OptimizeLoss', loss)
+    if mode in (Modes.TRAIN, Modes.EVAL):
+        global_step = tf.contrib.framework.get_or_create_global_step()
+        label_indices = tf.cast(labels, tf.int32)
+        loss = tf.losses.softmax_cross_entropy(
+                onehot_labels=tf.one_hot(label_indices, depth=10), logits=logits)
+        tf.summary.scalar('OptimizeLoss', loss)
 
-  if mode == Modes.INFER:
-    predictions = {
-        'classes': predicted_indices,
-        'probabilities': probabilities
-    }
-    export_outputs = {
-        'prediction': tf.estimator.export.PredictOutput(predictions)
-    }
-    return tf.estimator.EstimatorSpec(
-        mode, predictions=predictions, export_outputs=export_outputs)
+    if mode == Modes.INFER:
+        predictions = {
+                'classes': predicted_indices,
+                'probabilities': probabilities
+        }
+        export_outputs = {
+                'prediction': tf.estimator.export.PredictOutput(predictions)
+        }
+        return tf.estimator.EstimatorSpec(
+                mode, predictions=predictions, export_outputs=export_outputs)
 
-  if mode == Modes.TRAIN:
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
-    train_op = optimizer.minimize(loss, global_step=global_step)
-    return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
+    if mode == Modes.TRAIN:
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(loss, global_step=global_step)
+        return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
 
-  if mode == Modes.EVAL:
-    eval_metric_ops = {
-        'accuracy': tf.metrics.accuracy(label_indices, predicted_indices)
-    }
-    return tf.estimator.EstimatorSpec(
-        mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    if mode == Modes.EVAL:
+        eval_metric_ops = {
+                'accuracy': tf.metrics.accuracy(label_indices, predicted_indices)
+        }
+        return tf.estimator.EstimatorSpec(
+                mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 def _inception_resnet_v1_model_fn(features, labels, mode):
     # Input Layer
@@ -175,15 +175,15 @@ def _inception_resnet_v1_model_fn(features, labels, mode):
     labels = ops.convert_to_tensor(labels, dtype=tf.int32)
     range_size = array_ops.shape(labels)[0]
     learning_rate_placeholder = tf.placeholder(tf.float32, name='learning_rate')
-        
+            
     phase_train_placeholder = tf.placeholder(tf.bool, name='phase_train')
     labels_placeholder = tf.placeholder(tf.int32, shape=(None,1), name='labels')
     # Build the inference graph
     prelogits, _ = network.inference(input_layer, 0.4, phase_train=phase_train_placeholder, bottleneck_layer_size=EMBEDDING_SIZE, weight_decay=5e-4)
     logits = slim.fully_connected(prelogits, NUMBER_OF_CLASSES, activation_fn=None, 
-            weights_initializer=slim.initializers.xavier_initializer(WEIGHT_DECAY), 
-            weights_regularizer=slim.l2_regularizer(WEIGHT_DECAY),
-            scope='Logits', reuse=False)
+                    weights_initializer=slim.initializers.xavier_initializer(WEIGHT_DECAY), 
+                    weights_regularizer=slim.l2_regularizer(WEIGHT_DECAY),
+                    scope='Logits', reuse=False)
 
     embeddings = tf.nn.l2_normalize(prelogits, 1, 1e-10, name='embeddings')
 
@@ -211,46 +211,44 @@ def _inception_resnet_v1_model_fn(features, labels, mode):
     regularization_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
     total_loss = tf.add_n([cross_entropy_mean] + regularization_losses, name='total_loss')
     # Define operations
-	if mode in (Modes.INFER, Modes.EVAL):
-		predicted_indices = tf.argmax(input=logits, axis=1)
-		probabilities = tf.nn.softmax(logits, name='softmax_tensor')
+    if mode in (Modes.INFER, Modes.EVAL):
+        predicted_indices = tf.argmax(input=logits, axis=1)
+        probabilities = tf.nn.softmax(logits, name='softmax_tensor')
 
-	if mode in (Modes.TRAIN, Modes.EVAL):
-		global_step = tf.contrib.framework.get_or_create_global_step()
-		label_indices = tf.cast(labels, tf.int32)
-		loss = tf.losses.softmax_cross_entropy(onehot_labels=tf.one_hot(label_indices, depth=10), logits=logits)
-		tf.summary.scalar('OptimizeLoss', loss)
+    if mode in (Modes.TRAIN, Modes.EVAL):
+        global_step = tf.contrib.framework.get_or_create_global_step()
+        label_indices = tf.cast(labels, tf.int32)
+        loss = tf.losses.softmax_cross_entropy(onehot_labels=tf.one_hot(label_indices, depth=10), logits=logits)
+        tf.summary.scalar('OptimizeLoss', loss)
 
-	if mode == Modes.INFER:
-		predictions = {
-		    'classes': predicted_indices,
-		    'probabilities': probabilities
-		}
-		export_outputs = {
-		    'prediction': tf.estimator.export.PredictOutput(predictions)
-		}
-		return tf.estimator.EstimatorSpec(mode, predictions=predictions, export_outputs=export_outputs)
+    if mode == Modes.INFER:
+        predictions = {
+                'classes': predicted_indices,
+                'probabilities': probabilities
+        }
+        export_outputs = {
+                'prediction': tf.estimator.export.PredictOutput(predictions)
+        }
+        return tf.estimator.EstimatorSpec(mode, predictions=predictions, export_outputs=export_outputs)
 
-	if mode == Modes.TRAIN:
-		optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
-		train_op = optimizer.minimize(loss, global_step=global_step)
-		return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
+    if mode == Modes.TRAIN:
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(loss, global_step=global_step)
+        return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
 
-	if mode == Modes.EVAL:
-		eval_metric_ops = {
-		    'accuracy': tf.metrics.accuracy(label_indices, predicted_indices)
-		}
-		return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    if mode == Modes.EVAL:
+        eval_metric_ops = {'accuracy': tf.metrics.accuracy(label_indices, predicted_indices)}
+        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
 
 def build_estimator(model_dir):
-  return tf.estimator.Estimator(
-      model_fn=_inception_resnet_v1_model_fn,
-      model_dir=model_dir,
-      config=tf.contrib.learn.RunConfig(save_checkpoints_secs=180))
+    return tf.estimator.Estimator(
+            model_fn=_inception_resnet_v1_model_fn,
+            model_dir=model_dir,
+            config=tf.contrib.learn.RunConfig(save_checkpoints_secs=180))
 
 
 def serving_input_fn():
-  inputs = {'inputs': tf.placeholder(tf.float32, [None, IMAGE_FLATTENED_SIZE])}
-  return tf.estimator.export.ServingInputReceiver(inputs, inputs)
+    inputs = {'inputs': tf.placeholder(tf.float32, [None, IMAGE_FLATTENED_SIZE])}
+    return tf.estimator.export.ServingInputReceiver(inputs, inputs)
