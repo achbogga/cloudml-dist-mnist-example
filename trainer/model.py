d149f0131a0d4a5685123ecd32b126f67b582cff
Achyut Boggaram <achyut.boggaram@clobotics.com>
2019-01-31 11:44:10 -0800

    fix syntax errors

--- trainer/model.py
+++ trainer/model.py
@@ -214,38 +214,37 @@ def _inception_resnet_v1_model_fn(features, labels, mode):
     # Calculate the total losses
     regularization_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
     total_loss = tf.add_n([cross_entropy_mean] + regularization_losses, name='total_loss')
+    # Define operations
+	if mode in (Modes.INFER, Modes.EVAL):
+		predicted_indices = tf.argmax(input=logits, axis=1)
+		probabilities = tf.nn.softmax(logits, name='softmax_tensor')
 
-  # Define operations
-  if mode in (Modes.INFER, Modes.EVAL):
-  	predicted_indices = tf.argmax(input=logits, axis=1)
-  	probabilities = tf.nn.softmax(logits, name='softmax_tensor')
+	if mode in (Modes.TRAIN, Modes.EVAL):
+		global_step = tf.contrib.framework.get_or_create_global_step()
+		label_indices = tf.cast(labels, tf.int32)
+		loss = tf.losses.softmax_cross_entropy(onehot_labels=tf.one_hot(label_indices, depth=10), logits=logits)
+		tf.summary.scalar('OptimizeLoss', loss)
 
-  if mode in (Modes.TRAIN, Modes.EVAL):
-  	global_step = tf.contrib.framework.get_or_create_global_step()
-  	label_indices = tf.cast(labels, tf.int32)
-  	loss = tf.losses.softmax_cross_entropy(onehot_labels=tf.one_hot(label_indices, depth=10), logits=logits)
-  	tf.summary.scalar('OptimizeLoss', loss)
+	if mode == Modes.INFER:
+		predictions = {
+		    'classes': predicted_indices,
+		    'probabilities': probabilities
+		}
+		export_outputs = {
+		    'prediction': tf.estimator.export.PredictOutput(predictions)
+		}
+		return tf.estimator.EstimatorSpec(mode, predictions=predictions, export_outputs=export_outputs)
 
-  if mode == Modes.INFER:
-  	predictions = {
-  	    'classes': predicted_indices,
-  	    'probabilities': probabilities
-  	}
-  	export_outputs = {
-  	    'prediction': tf.estimator.export.PredictOutput(predictions)
-  	}
-  	return tf.estimator.EstimatorSpec(mode, predictions=predictions, export_outputs=export_outputs)
+	if mode == Modes.TRAIN:
+		optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+		train_op = optimizer.minimize(loss, global_step=global_step)
+		return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
 
-  if mode == Modes.TRAIN:
-  	optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
-  	train_op = optimizer.minimize(loss, global_step=global_step)
-  	return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
-
-  if mode == Modes.EVAL:
-  	eval_metric_ops = {
-  	    'accuracy': tf.metrics.accuracy(label_indices, predicted_indices)
-  	}
-  	return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=eval_metric_ops)
+	if mode == Modes.EVAL:
+		eval_metric_ops = {
+		    'accuracy': tf.metrics.accuracy(label_indices, predicted_indices)
+		}
+		return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
 
